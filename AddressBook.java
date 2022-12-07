81588af4dd94c812fa5b73161791fe4069b47026
Vicky Verma <likevishalverma@gmail.com>
2022-12-07 14:28:15 +0530

    Add Merge branch 'master' into UC5-multipleContacts

---
+++ AddressBook.java
@@ -0,0 +1,17 @@
+package com.bridgelabz;
+
+import java.util.Scanner;
+
+public class AddressBook {
+	static Scanner sc = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		AddressBookMethods entry = new AddressBookMethods();
+
+		System.out.println("Welcome to Address Book Program in Java!");
+		System.out.println(
+				"You can create and modify Contacts in the Address Book with first/last names, address, city, state, zip, phone number and email and so on");
+
+		entry.manageAddressBook();
+	}
+}
